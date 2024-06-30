13350263029cf05877b6129b0889c68d16ef73cb
Babul <kbhagatb@gmail.com>
2024-06-30 19:39:57 +0530

    fotter description added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                Unleash the magic of cinema! Dive into top-rated movies, explore by genre, and make every movie night unforgettable. Your ultimate guide to the best films awaits!
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
